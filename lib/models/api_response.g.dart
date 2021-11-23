41c81143fc5929dd6cdfa5f9d47724bdb3dfeb9d
Konstantin Komkov <38656572+KonstantenKomkov@users.noreply.github.com>
2021-11-23 22:13:24 +0300

    Update g.dart file.

---
+++ lib/models/api_response.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ApiResponse<T> _$ApiResponseFromJson<T>(Map<String, dynamic> json) =>
+    ApiResponse<T>(
+      info: json['info'] == null
+          ? null
+          : Info.fromJson(json['info'] as Map<String, dynamic>),
+      results: json['results'],
+    );
+
+Map<String, dynamic> _$ApiResponseToJson<T>(ApiResponse<T> instance) =>
+    <String, dynamic>{
+      'info': instance.info,
+      'results': instance.results,
+    };
