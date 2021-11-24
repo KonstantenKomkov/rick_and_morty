ab91d49206b7bfa7bd660f10c498d0219d703989
Konstantin Komkov <38656572+KonstantenKomkov@users.noreply.github.com>
2021-11-24 23:25:17 +0300

    Added route for person list item. Split person list item on small widgets. Added additional info in person list item.

--- lib/pages/view/person_list_item.dart
+++ lib/pages/view/person_list_item.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty/models/person.dart';
-import 'package:rick_and_morty/pages/person_details_page.dart';
 
 class PersonListItem extends StatelessWidget {
   final Person person;
@@ -15,23 +14,34 @@ class PersonListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => PersonDetailsPage(
-              id: person.id,
-            ),
-          ),
-        ),
-      },
-      child: _buildPersonItem(context), //_buildRow(context, person),
+      onTap: () => Navigator.of(context).pushNamed(
+        '/person_details',
+        arguments: {
+          "id": person.id,
+        },
+      ),
+      child: _buildPersonItem(context),
     );
+  }
+
+  Color getStatusColor({
+    required String status,
+  }) {
+    switch (status) {
+      case "Alive":
+        return Colors.green;
+      case "Dead":
+        return Colors.red;
+      default:
+        return Colors.grey;
+    }
   }
 
   Widget _buildPersonItem(
     BuildContext context,
   ) {
     return Container(
+      height: 116.0,
       decoration: const BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.all(
@@ -53,64 +63,104 @@ class PersonListItem extends StatelessWidget {
         vertical: 4.0,
       ),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CircleAvatar(
-              radius: 48.0,
-              backgroundImage: NetworkImage(person.image),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                left: 16.0,
-              ),
-              child: Text(
-                person.name,
-                overflow: TextOverflow.ellipsis,
-                softWrap: true,
-                maxLines: 2,
-                style: const TextStyle(
-                  fontSize: 21.0,
-                ),
-              ),
-            ),
-          ),
+          _buildCardImage(context),
+          _buildCardInfo(context),
         ],
       ),
     );
   }
 
-  Widget _buildRow(BuildContext context, Person person) {
-    return Card(
-      margin: const EdgeInsets.symmetric(
-        vertical: 2.0,
-        horizontal: 5.0,
-      ),
-      color: person.status == 'Dead' ? Colors.black54 : Colors.white24,
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CircleAvatar(
-              backgroundImage: NetworkImage(person.image),
-            ),
-          ),
-          Flexible(
-            child: Text(
-              person.name,
-              overflow: TextOverflow.ellipsis,
-              softWrap: true,
-              maxLines: 1,
-              style: TextStyle(
-                fontSize: 25,
-                color: person.status == 'Dead' ? Colors.white70 : Colors.black,
-              ),
-            ),
-          ),
-        ],
+  Widget _buildCardImage(
+    BuildContext context,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: CircleAvatar(
+        radius: 50.0,
+        backgroundImage: NetworkImage(person.image),
       ),
     );
+  }
+
+  Widget _buildCardInfo(
+    BuildContext context,
+  ) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildPersonName(context),
+            Padding(
+              padding: const EdgeInsets.only(
+                bottom: 4.0,
+              ),
+              child: _buildStatusRow(context),
+            ),
+            ..._buildPersonLocation(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPersonName(
+    BuildContext context,
+  ) {
+    return Text(
+      person.name,
+      overflow: TextOverflow.ellipsis,
+      softWrap: true,
+      maxLines: 1,
+      style: const TextStyle(
+        fontSize: 21.0,
+      ),
+    );
+  }
+
+  Widget _buildStatusRow(
+    BuildContext context,
+  ) {
+    return Row(
+      children: [
+        Container(
+          width: 10.0,
+          height: 10.0,
+          margin: const EdgeInsets.all(4.0),
+          decoration: BoxDecoration(
+            color: getStatusColor(
+              status: person.status,
+            ),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+        ),
+        Text(
+          "${person.species} - ${person.status}",
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ],
+    );
+  }
+
+  List<Widget> _buildPersonLocation(
+    BuildContext context,
+  ) {
+    return [
+      const Padding(
+        padding: EdgeInsets.only(
+          bottom: 4.0,
+        ),
+        child: Text("Origin:"),
+      ),
+      Text(
+        person.origin.name,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      )
+    ];
   }
 }
