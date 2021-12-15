00fe1ad04819291c8f3ae51f4f64b51ba74b55ce
Konstantin Komkov <38656572+KonstantenKomkov@users.noreply.github.com>
2021-12-15 23:59:26 +0300

    Add freezed library.

--- lib/models/person.dart
+++ lib/models/person.dart
@@ -1,41 +1,27 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:rick_and_morty/models/location.dart';
 import 'package:rick_and_morty/models/origin.dart';
 
+part 'person.freezed.dart';
 part 'person.g.dart';
 
-@JsonSerializable()
-class Person {
-  final int id;
-  final String name;
-  final String status;
-  final String species;
-  final String type;
-  final String gender;
-  final Origin origin;
-  final Location location;
-
-  final String image;
-
-  final List<String> episode;
-  final String url;
-  final String created;
-
-  Person({
-    required this.id,
-    required this.name,
-    required this.status,
-    required this.species,
-    required this.type,
-    required this.gender,
-    required this.origin,
-    required this.location,
-    required this.image,
-    required this.episode,
-    required this.url,
-    required this.created,
-  });
-
+@freezed
+class Person with _$Person {
+  factory Person({
+    required int id,
+    required String name,
+    required String status,
+    required String species,
+    required String type,
+    required String gender,
+    required Origin origin,
+    required Location location,
+    required String image,
+    required List<String> episode,
+    required String url,
+    required String created,
+  }) = _Person;
   factory Person.fromJson(Map<String, dynamic> json) => _$PersonFromJson(json);
   Map<String, dynamic> toJson() => _$PersonToJson(this);
 }
