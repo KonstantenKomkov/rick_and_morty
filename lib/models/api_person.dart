a0100bf6c272bacde089f715adc9dc55c65c60d7
Konstantin Komkov <38656572+KonstantenKomkov@users.noreply.github.com>
2021-12-22 23:35:12 +0300

    Method for loading persons list, which return decoded List<Person> data in ApiResponse.

---
+++ lib/models/api_person.dart
@@ -0,0 +1,46 @@
+import 'package:bot_toast/bot_toast.dart';
+import 'package:rick_and_morty/models/api_response.dart';
+import 'package:rick_and_morty/models/info.dart';
+import 'package:rick_and_morty/models/person.dart';
+import 'package:rick_and_morty/response_methods.dart';
+
+Future<ApiResponse> loadPersonsList({
+  required bool loadNextPage,
+  Info? info,
+}) async {
+  final ApiResponse response = await getListData(
+    undecodedPath: 'api/character',
+    loadNextPage: loadNextPage,
+    info: info,
+  );
+
+  if (response.isError) {
+    return response;
+  } else {
+    if (response.results != null) {
+      try {
+        final List<dynamic> decodedData = response.results! as List<dynamic>;
+        final List<Person> persons = decodedData
+            .map(
+              (person) => Person.fromJson(person as Map<String, dynamic>),
+            )
+            .toList();
+        return ApiResponse(
+          info: response.info,
+          results: persons,
+        );
+      } catch (e) {
+        return ApiResponse(
+          info: response.info,
+          results: response.results,
+          error: e.toString(),
+        );
+      }
+    } else {
+      ApiResponse(
+        info: response.info,
+        results: response.results,
+      );
+    }
+  }
+}
