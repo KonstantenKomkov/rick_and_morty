06805220e8e8e37cff02a6e21f4dd5d512305e67
Konstantin Komkov <38656572+KonstantenKomkov@users.noreply.github.com>
2021-12-28 23:24:10 +0300

    Add MultiProvider for changing ThemeData when user changed Brightness on phone.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:bot_toast/bot_toast.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:rick_and_morty/app_theme.dart';
+import 'package:provider/provider.dart';
 import 'package:rick_and_morty/pages/location_details_page.dart';
 import 'package:rick_and_morty/pages/not_found_page.dart';
 import 'package:rick_and_morty/pages/person_details_page.dart';
 import 'package:rick_and_morty/pages/person_list_page.dart';
+import 'package:rick_and_morty/providers/theme_providers.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,8 +14,54 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ThemeProvider(),
+        )
+      ],
+      child: App(),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> with WidgetsBindingObserver {
+  Brightness? brightness;
+
+  // https://stackoverflow.com/questions/58260648/how-to-listen-for-changes-to-platformbrightness-in-flutter
+  @override
+  void initState() {
+    WidgetsBinding.instance?.addObserver(this);
+    brightness = WidgetsBinding.instance?.window.platformBrightness;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    setState(() {
+      brightness = WidgetsBinding.instance?.window.platformBrightness;
+    });
+    super.didChangePlatformBrightness();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var themeProvider = Provider.of<ThemeProvider>(context);
     return MaterialApp(
-      theme: appTheme,
+      theme: themeProvider.current(brightness),
       title: 'Rick and Morty',
       home: const PersonListPage(),
       builder: BotToastInit(),
